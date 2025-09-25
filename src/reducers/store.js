ea1af9ef7555f85f5ab7965eac75976f6f7a89a8
Nandini sandip mane <121483153+8767024424@users.noreply.github.com>
2025-09-25 15:15:27 +0530

    Add files via upload

---
+++ src/reducers/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import anecdoteReducer from './reducers/anecdoteSlice';
+
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer
+  }
+});
+
+export default store;
